8b2686916a4093904eaa713e15c1775266da79ff
Sagar-Patil-C94 <sagarp@ivoyant.com>
2023-12-27 21:47:46 +0530

    Added Functionality to convert Result Set to XLSX

--- src/main/java/com/sql/DbToCsv.java
+++ src/main/java/com/sql/DbToCsv.java
@@ -2,69 +2,30 @@ package com.sql;
 
 import com.ivoyant.mysql.ConnectionHandler;
 import com.ivoyant.mysql.CustomLogger;
-import com.ivoyant.mysql.DataOperations;
 import com.opencsv.CSVWriter;
 
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
+import static com.sql.DbResult.connectionMap;
+import static com.sql.DbResult.key;
+
 public class DbToCsv {
-    public static Scanner scanner = new Scanner(System.in);
-    public static Map<String, Connection> connectionMap = new HashMap<>();
-    public static String databaseType;
 
     public static void getCsv() throws IOException, SQLException {
-        System.out.print("Please SQL Type\n1.MySQL\n2.Postgres : ");
-        int choice = scanner.nextInt();
-        scanner.nextLine();
-        switch (choice) {
-            case 1:
-                databaseType = "mysql";
-                CustomLogger.info("User Selected " + databaseType);
-                break;
-            case 2:
-                databaseType = "postgresql";
-                CustomLogger.info("User Selected " + databaseType);
-                break;
-            default:
-                CustomLogger.error("User Selected Invalid Option");
-        }
-        System.out.print("Please Enter Host Name : ");
-        String hostName = scanner.nextLine();
-        System.out.print("Please Enter Port Number : ");
-        int portNumber = scanner.nextInt();
-        scanner.nextLine();
-        System.out.print("Please Enter Database Name : ");
-        String databaseName = scanner.nextLine();
-        System.out.print("Please Enter Username : ");
-        String username = scanner.nextLine();
-        System.out.print("Please Enter Password : ");
-        String password = scanner.nextLine();
-        String key = username + "_" + databaseName;
-        if (connectionMap.containsKey(key)) {
-            CustomLogger.warn("Connection Already Present!!!");
-        } else {
-            connectionMap.put(key, ConnectionHandler.connect(hostName, databaseType, portNumber, databaseName, username, password));
-            CustomLogger.info("Connection Established!!!");
-        }
-        System.out.print("Enter Table Name : ");
-        String tableName = scanner.nextLine();
-        System.out.print("Enter Column List : ");
-        String columnList = scanner.nextLine();
+        ResultSet resultSet = DbResult.getResult();
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Enter file Name : ");
         String filePath = scanner.nextLine();
-        ResultSet resultSet = DataOperations.displayData(connectionMap.get(key), columnList, tableName);
         CSVWriter csvWriter = new CSVWriter(new FileWriter("src/main/resources/" + filePath + ".csv"));
         csvWriter.writeAll(resultSet, true);
         csvWriter.close();
         ConnectionHandler.disconnect(connectionMap.get(key));
         CustomLogger.info("Converted To CSV Successfully!!");
+        System.out.println("CSV file has been created successfully!");
     }
 
     public static void main(String[] args) throws SQLException, IOException {
